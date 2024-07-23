43dbd3773299507b5df03f78fceaead47180acec
ninanee <ni.yun@outlook.com>
2024-07-23 11:39:57 -0400

    Initial Commit

---
+++ SwiftUIAd/SwiftUIAdApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdApp.swift
+//  SwiftUIAd
+//
+//  Created by Yun Ni on 2024-07-23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
